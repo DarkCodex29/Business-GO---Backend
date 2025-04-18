a614e833c66a2ba2c01ae9282e32b8b54d022c31
Gianpierre Mio <gianxs296@gmail.com>
2025-04-18 17:08:02 -0500

    Actualizar el módulo principal para reorganizar la importación de módulos de infraestructura y aplicación, mejorando la legibilidad y el orden. Se añaden comentarios para clarificar la estructura de los módulos y se ajusta el orden de importación según el menú de la aplicación.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { EmailModule } from './email/email.module';
-import { EmpresasModule } from './empresas/empresas.module';
-import { FilesModule } from './files/files.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ProductsModule } from './productos/products.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
 import { ClientsModule } from './clients/clients.module';
+import { EmpresasModule } from './empresas/empresas.module';
+import { FilesModule } from './files/files.module';
+import { ProductsModule } from './productos/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
-    // Módulos de Infraestructura
+    // Módulos de Infraestructura (no visibles en Swagger)
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    EmailModule,
     PrismaModule,
+    EmailModule,
 
-    // Módulos de la Aplicación (orden alfabético)
-    AuthModule, // Autenticación
-    ClientsModule, // Clientes
-    EmpresasModule, // Empresas
-    FilesModule, // Archivos
-    ProductsModule, // Productos
-    RolesModule, // Roles
-    UsersModule, // Usuarios
+    // Módulos de la Aplicación (orden según menú)
+    AuthModule, // 1. Autenticación
+    RolesModule, // 2. Permisos
+    UsersModule, // 3. Usuarios
+    ClientsModule, // 4. Clientes
+    EmpresasModule, // 5. Empresas
+    FilesModule, // 6. Archivos
+    ProductsModule, // 7. Productos (incluye Stock, Precios, etc.)
   ],
 })
 export class AppModule {}
